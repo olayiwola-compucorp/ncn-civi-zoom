e8cc9c7d249231d96be9649bd435e199935c1e8a
Mathieu Lu <mathieu@symbiotic.coop>
2023-09-10 14:16:41 -0400

    Update civix

--- ncn_civi_zoom.php
+++ ncn_civi_zoom.php
@@ -22,15 +22,6 @@ function ncn_civi_zoom_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function ncn_civi_zoom_civicrm_xmlMenu(&$files) {
-  _ncn_civi_zoom_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -51,16 +42,6 @@ function ncn_civi_zoom_civicrm_postInstall() {
   CRM_NcnCiviZoom_Utils::forUpgrade1004();
   CRM_NcnCiviZoom_Utils::forUpgrade1006();
   CRM_NcnCiviZoom_Utils::forUpgrade1007();
-  _ncn_civi_zoom_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function ncn_civi_zoom_civicrm_uninstall() {
-  _ncn_civi_zoom_civix_civicrm_uninstall();
 }
 
 /**
@@ -73,15 +54,6 @@ function ncn_civi_zoom_civicrm_enable() {
 }
 
 /**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function ncn_civi_zoom_civicrm_disable() {
-  _ncn_civi_zoom_civix_civicrm_disable();
-}
-
-/**
  * Implements hook_civicrm_upgrade().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
@@ -89,73 +61,7 @@ function ncn_civi_zoom_civicrm_disable() {
 function ncn_civi_zoom_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   CRM_Civirules_Utils_Upgrader::insertActionsFromJson(__DIR__ . '/civirules_actions.json');
 
-  return _ncn_civi_zoom_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function ncn_civi_zoom_civicrm_managed(&$entities) {
-  _ncn_civi_zoom_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function ncn_civi_zoom_civicrm_caseTypes(&$caseTypes) {
-  _ncn_civi_zoom_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function ncn_civi_zoom_civicrm_angularModules(&$angularModules) {
-  _ncn_civi_zoom_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function ncn_civi_zoom_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _ncn_civi_zoom_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function ncn_civi_zoom_civicrm_entityTypes(&$entityTypes) {
-  _ncn_civi_zoom_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function ncn_civi_zoom_civicrm_themes(&$themes) {
-  _ncn_civi_zoom_civix_civicrm_themes($themes);
+  return;
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
@@ -458,7 +364,6 @@ function ncn_civi_zoom_civicrm_postProcess($formName, $form) {
   }
 }
 
-
 function ncn_civi_zoom_civicrm_pageRun(&$page) {
   $pageName = $page->getVar('_name');
   if ($pageName == 'CRM_Event_Page_EventInfo') {
@@ -469,7 +374,6 @@ function ncn_civi_zoom_civicrm_pageRun(&$page) {
     ));
   }
 }
-
 
 function ncn_civi_zoom_civicrm_links($op, $objectName, $objectId, &$links, &$mask, &$values){
   //CRM_Core_Error::debug_var('op', $op);
